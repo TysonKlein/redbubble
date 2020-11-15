fcf1d82ad10e99e81638b3e119092dbdc4520d5e
Tyson Klein <Tyson.klein@hotmail.com>
2020-11-15 12:35:44 -0600

    Updated Data, added png outputs to readme

---
+++ code/distribution-analysis.R
@@ -0,0 +1,104 @@
+library(fitdistrplus)#for distribution fitting
+library(VGAM)#for dagum functions
+library(SpatialExtremes)#for gev functions
+
+#Setting the correct path
+if (is.null(rstudioapi::getActiveDocumentContext()))
+{
+  script.dir <- dirname(sys.frame(1)$ofile)
+  wd <- script.dir
+  setwd(wd)
+}else
+{
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+}
+setwd("..")
+
+#Load in the most recent data to analyze
+load(file = "rda/daily-data.rda")
+
+for (I in 1:2) {
+#Selecting the sample required for fitting a distribution
+  if(I == 1)
+  {
+    fitting.sample <- daily.data$sales/daily.data$mean.sales
+  }
+  if(I == 2)
+  {
+    fitting.sample <- daily.data$users/daily.data$mean.users
+  }
+#this can also be done for Sales, orders or units with similar results
+
+#Removing 0 values and replacing them with practically identical ones
+fitting.sample[fitting.sample == 0] <- 0.0001
+#This is required to fit some distributions
+
+#Fitting the 5 possible preselected distributions, as determined by familiarity
+fit.norm <- fitdist(fitting.sample, distr = "norm", method = "mge")
+fit.gamma <- fitdist(fitting.sample, distr = "gamma", lower = c(0,0), method = "mge")
+fit.weibull <- fitdist(fitting.sample, distr = "weibull", lower = c(0,0), method = "mge")
+fit.dagum <- fitdist(fitting.sample, distr = "dagum", start = list(scale = 1, shape1.a = 1, shape2.p = 1), method = 'mge')
+fit.gev <- fitdist(fitting.sample, distr = "gev", start = list(loc = 1, scale = 0.25, shape =1), method = "mge")
+
+#Performing a Kolmogorov-Smirnov fitness test for all possible distributions
+ks.test.norm <- ks.test(fitting.sample, "pnorm", mean = fit.norm$estimate["mean"], sd = fit.norm$estimate["sd"])
+ks.test.gamma <- ks.test(fitting.sample, "pgamma", shape = fit.gamma$estimate["shape"], rate = fit.gamma$estimate["rate"])
+ks.test.weibull <- ks.test(fitting.sample, "pweibull", shape = fit.weibull$estimate["shape"], scale = fit.weibull$estimate["scale"])
+ks.test.dagum <- ks.test(fitting.sample, "pdagum", scale = fit.dagum$estimate["scale"], shape1.a = fit.dagum$estimate["shape1.a"], shape2.p = fit.dagum$estimate["shape2.p"])
+ks.test.gev <-  ks.test(fitting.sample, "pgev", loc = fit.gev$estimate["loc"], scale = fit.gev$estimate["scale"], shape = fit.gev$estimate["shape"])
+
+ks.results <- data.frame("Distribution" = 
+                           c("Normal", "Gamma", "Weibull", "Dagum", "GEV"),
+                         "Test Statistic" = 
+                           c(ks.test.norm$statistic, ks.test.gamma$statistic, ks.test.weibull$statistic, ks.test.dagum$statistic, ks.test.gev$statistic),
+                         "P-value" = 
+                           c(ks.test.norm$p.value, ks.test.gamma$p.value, ks.test.weibull$p.value, ks.test.dagum$p.value, ks.test.gev$p.value))
+#Results for the Kolmogorov-Smirnov fitness tests
+ks.results
+
+if(I == 1)
+  {
+    save(ks.results, file = "rda/ks-tests-sales.rda")
+    save(fit.dagum, file = "rda/best-fit-dist-sales.rda")
+    dagum.sales <- fit.dagum
+
+    # Compare fits 
+    fits.sales <- list(fit.norm, fit.gamma, fit.weibull, fit.dagum, fit.gev)
+
+    #Save fits for Users, can also be done for other factors
+    save(fits.sales, file = "rda/fitted-distributions-sales.rda")
+  }
+  if(I == 2)
+  {
+    save(ks.results, file = "rda/ks-tests-users.rda")
+    save(fit.dagum, file = "rda/best-fit-dist-users.rda")
+    dagum.users <- fit.dagum
+
+    # Compare fits 
+    fits.users <- list(fit.norm, fit.gamma, fit.weibull, fit.dagum, fit.gev)
+
+    #Save fits for Users, can also be done for other factors
+    save(fits.users, file = "rda/fitted-distributions-users.rda")
+  }
+
+}
+
+#Confidence intervals
+weekly <- data.frame(tot = daily.data$sales[7:nrow(daily.data)])
+weekly
+for (i in 1:nrow(weekly)) {
+  weekly$tot[i] <- sum(daily.data$sales[i:(i+7)])
+}
+plot(weekly$tot)
+
+CI.value <- 0.9
+daily.data <- daily.data %>%
+  mutate(users.CI.upper = mean.users*qdagum(1-(1-CI.value)/2, scale = dagum.users$estimate["scale"], shape1.a = dagum.users$estimate["shape1.a"], shape2.p = dagum.users$estimate["shape2.p"]),
+         users.CI.lower = mean.users*qdagum((1-CI.value)/2, scale = dagum.users$estimate["scale"], shape1.a = dagum.users$estimate["shape1.a"], shape2.p = dagum.users$estimate["shape2.p"]),
+         sales.CI.upper = mean.sales*qdagum(1-(1-CI.value)/2, scale = dagum.sales$estimate["scale"], shape1.a = dagum.sales$estimate["shape1.a"], shape2.p = dagum.sales$estimate["shape2.p"]),
+         sales.CI.lower = mean.sales*qdagum((1-CI.value)/2, scale = dagum.sales$estimate["scale"], shape1.a = dagum.sales$estimate["shape1.a"], shape2.p = dagum.sales$estimate["shape2.p"]))
+
+#Save daily
+save(daily.data, file = "rda/daily-data.rda")
+write.csv(daily.data, file = "data/daily-data.csv")
+
---
+++ code/generate-plots.R
@@ -0,0 +1,162 @@
+library(readr)
+library(ggplot2)
+library(magrittr)
+library(lubridate)
+library(dplyr)
+library(scales)  
+require(reshape2)
+
+
+#Set the path to be correctt
+if (is.null(rstudioapi::getActiveDocumentContext()))
+{
+  script.dir <- dirname(sys.frame(1)$ofile)
+  wd <- script.dir
+  setwd(wd)
+}else setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("..")
+
+#Load the dataset for Daily plots
+load(file = "rda/daily-data.rda")
+
+#Set the plot theme for all following plots
+plot.theme <- theme(axis.text.x=element_text(angle=60, hjust=1),
+                    panel.background = element_rect(fill = "grey95"))
+
+#Plot for Daily Sales
+daily.sales.plot <- ggplot() + 
+  geom_point(data = daily.data, colour = 'red', size = 3, alpha = 0.3, aes(x=Date, y = sales)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Profit ($ CAD)", breaks = seq(0, (round(max(daily.data$sales-5)/10)+1)*10, 10)) +
+  plot.theme
+save(daily.sales.plot, file = "rda/daily-sales-plot.rda")
+save(daily.sales.plot, file = "png/daily-sales-plot.png")
+ggsave("png/daily-sales-plot.png", plot = daily.sales.plot)
+
+#Plot for Daily Sales + Mean sales + CI
+daily.sales.plot.A <- ggplot() + 
+  geom_point(data = daily.data, colour = 'red', size = 3, alpha = 0.3, aes(x=Date, y = sales)) +
+  geom_line(data = daily.data, colour = 'red', size = 1.5, aes(x=Date, y = mean.sales)) +
+  geom_line(data = daily.data, colour = 'red',alpha = 0.3, size = 1.5, aes(x=Date, y = sales.CI.upper)) +
+  geom_line(data = daily.data, colour = 'red',alpha = 0.3, size = 1.5, aes(x=Date, y = sales.CI.lower)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Profit ($ CAD)", breaks = seq(0, (round(max(daily.data$sales-5)/10)+1)*10, 10)) +
+  annotate("segment", x = as.Date("2019-04-27"), xend = as.Date("2018-11-24"), y = 110, yend = 104, colour = "black", size=1, arrow=arrow()) +
+  annotate("text", x = as.Date("2019-04-27"), y = 114, label = "Cyber Monday" , color="black", size=3 , fontface="bold") +
+  annotate("segment", x = as.Date("2019-04-27"), xend = as.Date("2019-12-02"), y = 110, yend = 93, colour = "black", size=1, arrow=arrow()) +
+  plot.theme
+save(daily.sales.plot.A, file = "rda/daily-sales-plot-A.rda")
+ggsave("png/daily-sales-plot-A.png", plot = daily.sales.plot.A)
+
+#Plot for Daily Users
+daily.users.plot <- ggplot() + 
+  geom_point(data = daily.data, colour = 'turquoise 4', size = 3, alpha = 0.3, aes(x=Date, y = users)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Users", breaks = seq(0, (round(max(daily.data$users-5)/10)+1)*10, 10)) +
+  plot.theme
+save(daily.users.plot, file = "rda/daily-users-plot.rda")
+ggsave("png/daily-users-plot.png", plot = daily.users.plot)
+
+#Plot for Daily Users adjusted
+daily.users.adjusted.plot <- ggplot() + 
+  geom_point(data = daily.data, colour = 'turquoise 4', size = 3, alpha = 0.3, aes(x=Date, y = users/mean.users)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Adjusted Users") +
+  plot.theme
+save(daily.users.adjusted.plot, file = "rda/daily-users-adjusted-plot.rda")
+
+#Plot for Daily Users + Mean users + CI
+daily.users.plot.A <- ggplot() + 
+  geom_point(data = daily.data, colour = 'turquoise 4', size = 3, alpha = 0.3, aes(x=Date, y = users)) +
+  geom_line(data = daily.data, colour = 'turquoise 4', size = 1.5, aes(x=Date, y = mean.users)) +
+  geom_line(data = daily.data, colour = 'turquoise 4',alpha = 0.3, size = 1.5, aes(x=Date, y = users.CI.upper)) +
+  geom_line(data = daily.data, colour = 'turquoise 4',alpha = 0.3, size = 1.5, aes(x=Date, y = users.CI.lower)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Users", breaks = seq(0, (round(max(daily.data$users-5)/10)+1)*10, 10)) +
+  plot.theme
+save(daily.users.plot.A, file = "rda/daily-users-plot-A.rda")
+
+#Plot for Daily orders + mean orders
+daily.orders.plot <- ggplot() + 
+  geom_point(data = daily.data, colour = 'orange 4', size = 3, alpha = 0.3, aes(x=Date, y = orders)) +
+  geom_line(data = daily.data, colour = 'orange 4', size = 1.5, aes(x=Date, y = mean.orders)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Orders", breaks = seq(0, (round(max(daily.data$orders-5)/10)+1)*10, 10)) +
+  plot.theme
+save(daily.orders.plot, file = "rda/daily-orders-plot.rda")
+
+#Plot for Daily units + mean units
+daily.units.plot <- ggplot() + 
+  geom_point(data = daily.data, colour = 'blue', size = 3, alpha = 0.3, aes(x=Date, y = units)) +
+  geom_line(data = daily.data, colour = 'blue', size = 1.5, aes(x=Date, y = mean.units)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "units", breaks = seq(0, (round(max(daily.data$units-5)/10)+1)*10, 10)) +
+  plot.theme
+save(daily.units.plot, file = "rda/daily-units-plot.rda")
+
+#Plot for Daily $ per Order
+daily.dollar.per.order.plot <- ggplot() + 
+  geom_line(data = daily.data, colour = "purple", size = 1.5, aes(x=Date, y = mean.sales/mean.orders)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Profit per Order", breaks = seq(0, 3, 0.5)) +
+  expand_limits(y = 0) +
+  plot.theme
+save(daily.dollar.per.order.plot, file = "rda/daily-dollar-per-order-plot.rda")
+
+#Plot for Daily $ per Order
+daily.dollar.per.user.plot <- ggplot() + 
+  geom_line(data = daily.data, colour = "purple", size = 1.5, aes(x=Date, y = mean.sales/mean.users)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Profit per Order", breaks = seq(0, 3, 0.5)) +
+  expand_limits(y = 0) +
+  plot.theme
+daily.dollar.per.user.plot
+
+#Plot for Daily Orders per User
+daily.order.per.user.plot <- ggplot() + 
+  geom_line(data = daily.data, colour = "turquoise 4", size = 1.5, aes(x=Date, y = mean.orders/mean.users)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Orders per User", breaks = seq(0, 1, 0.1)) +
+  expand_limits(y = 0) +
+  plot.theme
+save(daily.order.per.user.plot, file = "rda/daily-order-per-user-plot.rda")
+
+#Plot for Daily Units per Order
+daily.units.per.order.plot <- ggplot() + 
+  geom_line(data = daily.data, colour = "blue 4", size = 1.5, aes(x=Date, y = mean.units/mean.orders)) +
+  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y", minor_breaks = "1 month") +
+  scale_y_continuous(name = "Units per Order", breaks = seq(1, 1.5, 0.1)) +
+  expand_limits(y = 1) +
+  plot.theme
+save(daily.units.per.order.plot, file = "rda/daily-units-per-order-plot.rda")
+
+#Plot for units per order histogram
+load(file = "rda/order-data.rda")
+units.per.order.hist <- ggplot(order.data %>% filter(units < 10), aes(x = as.character(units))) +
+  geom_bar() +
+  xlab("Units in Order") + 
+  ylab("Orders") +
+  plot.theme + 
+  theme(axis.text.x=element_text(angle=0))
+save(units.per.order.hist, file = "rda/units-per-order-hist.rda")
+
+#Plot for unit profit histogram
+load(file = "rda/discounts.rda")
+load(file = "rda/price-stratified.rda")
+unit.profit.hist <- ggplot(,aes(x = price.stratified$Artists.Cut)) + 
+  geom_histogram(binwidth = 0.01) + 
+  xlab("Profit per Unit") + 
+  ylab("") +
+  scale_x_continuous(breaks = seq(2, 4.25, 0.25),labels = dollar_format(prefix = "$"))
+save(unit.profit.hist, file = "rda/unit-profit-hist.rda")
+  
+#Plot for unit profit histogram with discounts
+unit.profit.hist.with.discount <- ggplot(,aes(x = price.stratified$Artists.Cut)) + 
+  geom_histogram(binwidth = 0.01) + 
+  xlab("Profit per Unit") + 
+  ylab("") +
+  geom_vline(data = discounts, xintercept = discounts$cost,size = 1.1,alpha = 0.6, color = colorRampPalette(c("blue", "red"))(nrow(discounts))) +
+  geom_text(data = discounts, label = discounts$percent, y = 135,x = discounts$cost + 0.03, colour = colorRampPalette(c("blue", "red"))(nrow(discounts)), size = 4, angle = 90) +
+  scale_x_continuous(breaks = seq(2, 4.25, 0.25),labels = dollar_format(prefix = "$"))
+save(unit.profit.hist.with.discount, file = "rda/unit-profit-hist-with-discount.rda")
+
